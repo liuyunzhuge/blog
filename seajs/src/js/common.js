12efbfc9705408dec2958cf33a4cc454606bf188
felix <felix@3wcoffee.com>
2016-06-13 21:19:55 +0800

    调整seajs内的内容

--- seajs/src/js/common.js
+++ seajs/src/js/common.js
@@ -6,7 +6,6 @@ seajs.config({
         'lib': 'js/lib',
         'proj': 'js/proj'
     },
-    //对应lib/中的12个文件
     alias: {
         'jquery': 'lib/jquery.js?v=20160518',
         'bootstrap': 'lib/bootstrap.js?v=20160518',
