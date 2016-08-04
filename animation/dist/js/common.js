b9a3044b88404969a304136748f2f80243dbaa72
liuyunzhuge <imzengxf@163.com>
2016-08-04 16:59:37 +0800

    animation

--- animation/dist/js/common.js
+++ animation/dist/js/common.js
@@ -10,6 +10,7 @@ seajs.config({
     alias: {
         'swiper.jquery': 'lib/swiper.jquery.umd.js?v=20160721',
         'swiper': 'lib/swiper.js?v=20160721',
+        'iscroll': 'lib/iscroll-lite.js?v=20160721',
         'jquery': 'lib/zepto.js?v=20160721'
     }
 });
